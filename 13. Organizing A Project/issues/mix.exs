9ce530f344863e5164d983c9ae4b622693d4147a
Matt Caldwell <matt.caldwell@timmons.com>
2020-08-28 23:24:52 -0400

    work on prop tests n stuff

--- 13. Organizing A Project/issues/mix.exs
+++ 13. Organizing A Project/issues/mix.exs
@@ -3,11 +3,20 @@ defmodule Issues.MixProject do
 
   def project do
     [
-      app: :issues,
-      escript: escript_config(),
+      app: :issues,      
       version: "0.1.0",
+      name: "ISsues",
       elixir: "~> 1.10",
+      escript: escript_config(),
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -27,7 +36,8 @@ defmodule Issues.MixProject do
       { :httpoison, "~> 1.7.0" },
       { :poison, "~> 4.0.1" },
       { :ex_doc, "~> 0.22.2" },
-      { :earmark, "~> 1.4.10"}
+      { :earmark, "~> 1.4.10", override: true},
+      { :excoveralls, "~> 0.13.1", only: :test}
     ]
   end
 
