fcde05373b676dc4224d6c2e43d86916d9deefb4
Matt Caldwell <matt.caldwell@timmons.com>
2020-08-28 21:50:45 -0400

    work on ch 14

--- 14. Tooling/buggy/mix.exs
+++ 14. Tooling/buggy/mix.exs
@@ -1,10 +1,9 @@
-defmodule Issues.MixProject do
+defmodule Buggy.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :issues,
-      escript: escript_config(),
+      app: :buggy,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
@@ -24,16 +23,6 @@ defmodule Issues.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      { :httpoison, "~> 1.7.0" },
-      { :poison, "~> 4.0.1" },
-      { :ex_doc, "~> 0.22.2" },
-      { :earmark, "~> 1.4.10"}
-    ]
-  end
-
-  defp escript_config do
-    [
-      main_module: Issues.CLI
     ]
   end
 end
